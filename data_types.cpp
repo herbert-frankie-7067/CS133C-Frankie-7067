98f8dd6949d2df099acc337b4176edae7055aae2
Frankie Herbert <49249221+herbert-frankie-7067@users.noreply.github.com>
2019-10-11 11:40:55 -0700

    data_types.cpp
    This is assignment one in which passes threw my name and age using printf and an int.

---
+++ data_types.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/*
+For this code I used int assigned with age (a variable that can be changed to just about anything).
+for this assignment I printf - Hello my name is Frankie
+                    passing threw 'int' of my age 
+                    with printing out both results name first then age.
+*/
+
+  int main()
+{
+    
+    printf ("Hello My name is Frankie");  // this is a general print of any sentence added within ""
+  
+     int age = 30;  // here i am creating an int statement which has been named age for me to enter my age to be displayed
+                    //this can be interchangable 
+     printf ("\n\nage: %d", age);  // this print statement which display 'age' then whatever number i have secretly entered
+
+
+    
+    return 0;
+}
